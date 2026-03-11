e5e651451c61883f510281c13e0c2a03e1dbad59
Ngongangromels <romels064@gmail.com>
2026-03-11 01:19:17 +0100

    auth auth

---
+++ app/componants/sign-up.tsx
@@ -0,0 +1,11 @@
+
+export default function SignUp (){
+    return (
+        <div>
+            <h1>Sign Up</h1>
+            <form action="/sign-up">
+                <input type="email" name="email" placeholder="Email" />
+            </form>
+        </div>
+    )
+}
